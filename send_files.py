f8471d9b13d45f87eb6be2ab381b871d9d907c83
danipemos <daniel.pmosquera@udc.es>
2025-05-05 15:42:55 +0000

    Minor fixes

--- send_files.py
+++ send_files.py
@@ -1,31 +1,18 @@
-import requests
+import gnupg
 
-# Crear los archivos de prueba
-file_data = {
-    "encrypted.txt": "This is an encrypted file.",
-    ".zip.txt": "This is a zip file.",
-    "a.txt": "This is a plain text file."
-}
+# Inicializa el objeto GPG
+gpg = gnupg.GPG()
 
-# Crear los archivos localmente
-for filename, content in file_data.items():
-    with open(filename, "w") as f:
-        f.write(content)
+gpg.gen_key_input()
 
-# URL del endpoint de la vista upload_file
-BASE_URL = "http://192.168.1.67:8000/monitorize/upload-file/"
-hostname = "sniffer"
+# Listar claves públicas
+public_keys = gpg.list_keys()
+print("Claves públicas:")
+for key in public_keys:
+    print(f"ID: {key['keyid']}, UIDs: {key['uids']}")
 
-# Enviar los archivos al servidor
-for filename in file_data.keys():
-    with open(filename, "rb") as file:
-        response = requests.post(
-            f"{BASE_URL}{hostname}/",
-            files={"file": file}
-        )
-
-    # Imprimir los resultados
-    print(f"Testing file: {filename}")
-    print(f"Response status code: {response.status_code}")
-    print(f"Response JSON: {response.json()}")
-    print("-" * 50)
+# Listar claves privadas (secretas)
+private_keys = gpg.list_keys(secret=True)
+print("\nClaves privadas:")
+for key in private_keys:
+    print(f"ID: {key['keyid']}, UIDs: {key['uids']}")
