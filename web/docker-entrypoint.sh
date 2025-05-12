145e5b81d81ddc49ea7cc432b3184996c4bf9520
daniel.pmosquera <100313413+danipemos@users.noreply.github.com>
2025-05-12 21:05:30 +0200

    Delete unnecessary files

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 echo "making migrations"
-python manage.py makemigrations
+python3 manage.py makemigrations
 
-echo "running migrations"
-python manage.py migrate
+echo "migrating"
+python3 manage.py migrate
 
 echo "creating superuser"
+
 python manage.py shell <<EOF
 from users.models import OurUser
 if not OurUser.objects.filter(username='admin').exists():
-    OurUser.objects.create_superuser('admin', 'sniffer')
+    OurUser.objects.create_superuser(
+        username='admin',
+        password='sniffer',)
 EOF
+daphne -b 0.0.0.0 -p 8000 web.asgi:application
